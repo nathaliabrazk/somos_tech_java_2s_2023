87b530f17c949389aad04cdfba69366b2668d62a
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-09-21 21:31:12 -0300

    exemplos

---
+++ aula02/Aula02.java
@@ -0,0 +1,28 @@
+package aula02;
+
+public class Aula02{
+    public static void main(String[]args){
+
+        //tipos numéricos
+        int idade = 30;
+        double altura = 1.75;
+        float peso = 70.5f;
+        long populacional = 2458866550L;
+
+        //tipos de texto
+        char letra = 'b';
+        String nome = "Joao";
+        
+        //tipos logicos
+        boolean ehMenina = true;
+
+        int x = 5;
+        int y = 3;
+        int soma = x + y;
+        System.out.println(soma);
+
+        String nomeCompleto = nome + "Silva";
+        System.out.println("O nome completo é: " + nomeCompleto);
+
+    }
+}
---
+++ aula02/CalculaMedia.java
@@ -0,0 +1,24 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class CalculaMedia {
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //primeiro valor
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        //segundo valor
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        double media = (nota1 + nota2)/2;
+
+        System.out.println("A média é: " + media);
+
+    }
+}
